34c758695f2073fe55a3fe28bdaf852e79e8c589
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-03-16 15:15:43 -0600

    installs dapper

--- db-setup.sql
+++ db-setup.sql
@@ -1,10 +1,16 @@
-using bugloggerapi;
+USE bugloggerapi;
 
--- CREATE TABLE bugs (
---   id int NOT NULL AUTO_INCREMENT,
---   isClosed TINYINT,
---   description VARCHAR(255) NOT NULL,
---   title VARCHAR(255) NOT NULL,
---   reportedBy VARCHAR(255) NOT Null,
---   PRIMARY KEY (id)
--- );
+CREATE TABLE bugs
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
+  isClosed TINYINT,
+  description VARCHAR
+  (255) NOT NULL,
+  title VARCHAR
+  (255) NOT NULL,
+  reportedBy VARCHAR
+  (255) NOT Null,
+  PRIMARY KEY
+  (id)
+);
